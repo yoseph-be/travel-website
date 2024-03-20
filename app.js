937b2072a5cee71a20fba2505dd4772268ee4daa
Yoseph Geleta <yosephbeyene1@gmail.com>
2024-03-20 11:58:03 +0300

    Added GSAP

---
+++ app.js
@@ -0,0 +1,28 @@
+let controller;
+let slideScene;
+
+function animateSlides() {
+    //init controller
+    controller = new ScrollMagic.Controller();
+    //select some things
+    const sliders = document.querySelectorAll('.slide');
+    const nav = document.querySelector('.nav-header');
+    //loop over each slide
+    sliders.forEach(slide => {
+        const revealImg = slide.querySelector('.reveal-img');
+        const img = slide.querySelector('img');
+        const revealText = slide.querySelector('.reveal-text');
+        //GSAP
+
+        const slideTl = gsap.timeline({
+            defaults: { duratoin: 1, ease: "power2.inOut" }
+        });
+        slideTl.fromTo(revealImg, { x: '0%' }, { x: '100%' })
+        slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=0.75")
+        slideTl.fromTo(revealText, { x: '0%' }, { x: '100%' }, '-=0.3')
+        slideTl.fromTo(nav, { y: '-100%' }, { y: '0%' }, '-=0.2')
+    });
+
+}
+
+animateSlides();
