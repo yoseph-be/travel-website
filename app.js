c0a4794afcc928e325f9b091cfcbdefa22b6b117
Yoseph Geleta <yosephbeyene1@gmail.com>
2024-03-20 20:32:13 +0300

    Scroll Animation Overload.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let controller;
 let slideScene;
+let pageScene;
 
 function animateSlides() {
     //init controller
@@ -15,12 +16,51 @@ function animateSlides() {
         //GSAP
 
         const slideTl = gsap.timeline({
-            defaults: { duratoin: 1, ease: "power2.inOut" }
+            defaults: { duratoin: 0.75, ease: "power2.inOut" }
         });
         slideTl.fromTo(revealImg, { x: '0%' }, { x: '100%' })
         slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=0.75")
         slideTl.fromTo(revealText, { x: '0%' }, { x: '100%' }, '-=0.3')
         slideTl.fromTo(nav, { y: '-100%' }, { y: '0%' }, '-=0.2')
+
+        //create scene
+        slideScene = new ScrollMagic.Scene({
+            triggerElement: slide,
+            triggerHook: 0.25,
+            reverse: false
+        })
+
+        .setTween(slideTl)
+            .addIndicators({
+                colorStart: 'white',
+                colorTrigger: 'white',
+                name: 'slide'
+            })
+            .addTo(controller);
+        //New Animation
+        const pageTl = gsap.timeline();
+
+        pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 })
+
+
+        //Create new scene
+
+        pageScene = new ScrollMagic.Scene({
+                triggerElement: slide,
+                duration: '100%',
+                triggerHook: 0
+
+            })
+            .setPin(slide, { pushFollowers: false })
+            .setTween(pageTl)
+            .addIndicators({
+                colorStart: 'white',
+                colorTrigger: 'white',
+                name: 'page',
+                indent: 200
+            })
+
+        .addTo(controller)
     });
 
 }
