ce56cd3c8b9bd90d0debe6f597903f70fd0258cf
Yoseph Geleta <yosephbeyene1@gmail.com>
2024-03-23 18:46:45 +0300

    Added the toggle burger animation.

--- app.js
+++ app.js
@@ -65,4 +65,56 @@ function animateSlides() {
 
 }
 
+const mouse = document.querySelector('.cursor');
+const mouseTxt = mouse.querySelector('span');
+const burger = document.querySelector('.burger');
+
+function cursor(e) {
+    mouse.style.top = e.pageY + "px";
+    mouse.style.left = e.pageX + "px";
+}
+
+function activeCursor(e) {
+    const item = e.target;
+    if (item.id === 'logo' || item.classList.contains('burger')) {
+        mouse.classList.add('nav-active');
+    } else {
+        mouse.classList.remove('nav-active');
+    }
+
+    if (item.classList.contains('explore')) {
+        mouse.classList.add('explore-active');
+        mouseTxt.innerText = 'Tap';
+        gsap.to('.title-swipe', 1, { y: "0%" });
+    } else {
+        mouse.classList.remove('explore-active');
+        mouseTxt.innerText = '';
+        gsap.to('.title-swipe', 1, { y: "100%" });
+
+    }
+}
+
+function navToggle(e) {
+    if (!e.target.classList.contains("active")) {
+        e.target.classList.add("active");
+        gsap.to('.line1', 0.5, { rotate: '45', y: 5, background: "black" });
+        gsap.to('.line2', 0.5, { rotate: '-45', y: -5, background: "black" });
+        gsap.to('#logo', 0.5, { color: "black" });
+        gsap.to('.nav-bar', 1, { clipPath: 'circle(2500px at 100% -10%)' });
+        document.body.classList.add("hide");
+    } else {
+        e.target.classList.remove("active");
+        gsap.to('.line1', 0.5, { rotate: '0', y: 0, background: "white" });
+        gsap.to('.line2', 0.5, { rotate: '0', y: 0, background: "white" });
+        gsap.to('#logo', 0.5, { color: "black" });
+        gsap.to('.nav-bar', 1, { clipPath: 'circle(50px at 100% -10%)' });
+        document.body.classList.remove("hide");
+    }
+}
+
+//event listeners
+burger.addEventListener('click', navToggle);
+window.addEventListener('mousemove', cursor);
+window.addEventListener('mouseover', activeCursor);
+
 animateSlides();
